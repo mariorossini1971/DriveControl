b3a5ee373d965422fd9085dc5f87286f1e4853c9
Mario <mrossini1971@gmail.com>
2024-09-24 18:17:35 +0200

    comit hecho

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AngularFireModule} from '@angular/fire/compat';
+import { AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import { environment} from 'src/environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+//import { firebaseConfig } from 'src/environments/environment';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFqrOUi_tEEAsvp4P4xCBKMcMVZHIm7pc",
+  authDomain: "ionicproject-d9cce.firebaseapp.com",
+  projectId: "ionicproject-d9cce",
+  storageBucket: "ionicproject-d9cce.appspot.com",
+  messagingSenderId: "243766640179",
+  appId: "1:243766640179:web:2ba71f12900c245863ad46",
+  measurementId: "G-ZVTKH33DS2"
+};
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [HttpClientModule,BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  AngularFireModule.initializeApp(firebaseConfig), AngularFirestoreModule,
+  AngularFireAuthModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,113 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Route, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+
+export class LoginPage implements OnInit {
+
+  public showPassword = false;
+  public passwordIcono = 'eye';
+  public email = '';
+  public password = '';
+
+  constructor(public fireAuth: AngularFireAuth, public router: Router, public alertController: AlertController) { }
+
+  public username: string = '';
+
+ngOnInit() {
+  const userEmail = this.email;
+  this.username = this.userName(userEmail);
+}
+
+  public login(){
+  let tempEmail = this.email;  // Variable temporal para guardar el email
+  let tempPassword = this.password;
+    this.fireAuth.signInWithEmailAndPassword(tempEmail, tempPassword)
+    .then(res =>{
+      console.log("he entrado");
+      const username = this.userName(this.email);
+    //  this.router.navigate(['/home',{ username: username }]);
+    //    this.router.navigate(['/home/'+ username]);
+    this.router.navigate(['/home/']);
+    })
+    .catch(async error =>{
+      if (error.code === 'auth/invalid-credential') { 
+        console.log("error al entrar: ",error.message);
+        const alert = await this.alertController.create({
+          message: 'Usuario no registrado',
+          buttons: [{
+            
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            this.email = '';
+            this.password = '';
+          }
+      },
+        {
+          text: 'Crear Cuenta',
+          role: 'confirm',
+          handler: () =>this.signup(tempEmail,tempPassword)
+        }]
+        });
+        await alert.present();
+      } else {
+        const alert = await this.alertController.create({
+          message: 'Email / Password \n Invalido',
+          buttons: ['Aceptar']
+        });
+        await alert.present();
+        this.email = '';
+        this.password = '';
+      }
+
+    });
+  }
+  
+  public signup(email:string, password:string){
+    this.fireAuth.createUserWithEmailAndPassword(email, password)
+    .then(async res =>{
+      const alert = await this.alertController.create({
+        message:  'Usuario creado con éxito',
+        buttons: ['Aceptar']
+    })
+    await alert.present();
+    })
+    .catch(async error =>{
+      const alert = await this.alertController.create({
+        message:  'Email / Password \n Invalido',
+        buttons: ['Aceptar']
+    })
+    await alert.present();
+    this.email = '';
+    this.password = '';
+    })
+  }
+
+  public userName(email: string): string {
+    const name = email.indexOf('@');
+    if (name !== -1) {
+      return email.substring(0, name);
+    }
+    return email;
+  }
+  public muestraPassword():void{
+     this.showPassword = !this.showPassword;
+    if(this.passwordIcono == 'eye'){
+      this.passwordIcono = 'eye-off';
+    }else {
+      this.passwordIcono = 'eye';
+    }
+  
+  }
+
+}
+
+
+
