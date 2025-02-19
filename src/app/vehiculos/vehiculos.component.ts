58a3693183aa9b48ca8c55bc3907285d3f92797d
mrossini1971 <mrossini1971@gmail.com>
2025-02-19 19:41:44 +0100

    muesto coches libres

--- src/app/vehiculos/vehiculos.component.ts
+++ src/app/vehiculos/vehiculos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { ApiService } from '../api.service';
 import { RouterLink } from '@angular/router';
 import { Route, Router } from '@angular/router';
@@ -9,9 +9,9 @@ import { Route, Router } from '@angular/router';
   templateUrl: './vehiculos.component.html',
   styleUrls: ['./vehiculos.component.scss']
 })
-export class VehiculosComponent implements OnInit {
+export class VehiculosComponent implements OnInit,OnChanges {
   vehiculos: any[] = [];
-  modeloSeleccionado: string = "uno";
+  modeloSeleccionado: string = "";
   vehiculoSeleccionado: any = null;
   idVehiculo: number = 0;
 
@@ -23,9 +23,16 @@ export class VehiculosComponent implements OnInit {
     });*/
     this.cargarDatosVehiculos();
   }
+
+  ngOnChanges(changes: SimpleChanges) {       //para actualizar datos si cambian 
+    this.cargarDatosVehiculos();
+  }
+
   cargarDatosVehiculos(){
     this.apiService.getVehiculos().subscribe((data: any[]) => {
-      this.vehiculos = data;
+     // this.vehiculos = data;
+     this.vehiculos = data.filter(vehiculo => vehiculo.disponible == true);   //filtro solo los disponibles
+      console.log(data);
     });
   }
   seleccionarVehiculo(vehiculo:any){
@@ -36,15 +43,14 @@ export class VehiculosComponent implements OnInit {
     }
 
   }
-
-  guardarModelo() {
+  guardarModelo() {     // en realidad solo me sirve el id
         if (this.vehiculoSeleccionado) {
             this.modeloSeleccionado = this.vehiculoSeleccionado.modelo;
             this.idVehiculo = this.vehiculoSeleccionado.id_vehiculo;
             this.apiService.idCoche = this.idVehiculo;
             console.log("Modelo guardado:", this.modeloSeleccionado);
             console.log("id guardado:", this.idVehiculo);
-            this.apiService.idCoche = this.idVehiculo;
+            this.apiService.idCoche = this.idVehiculo;     //guardo en la variable "comun"
             this.router.navigate(['/home/']);
         } else {
             console.log("No hay vehículo seleccionado.");
